6fec347de9291c3742a3bffc95db1f77bb070f5a
Lubana <homslubana@gmail.com>
2024-12-15 22:31:29 -0800

    Add project files.

---
+++ HairdresserProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HairdresserProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
