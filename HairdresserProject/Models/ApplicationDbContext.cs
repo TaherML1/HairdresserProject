5768c079025d430a69db8b68b5aa54c884118ad1
Lubana <homslubana@gmail.com>
2024-12-15 23:10:53 -0800

    Veritabanı bağlantısı düzeltildi

---
+++ HairdresserProject/Models/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HairdresserProject.Models
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Burada tablolarınızı tanımlayabilirsiniz
+        // public DbSet<ModelAdi> Tablolar { get; set; }
+    }
+}
--- HairdresserProject/Program.cs
+++ HairdresserProject/Program.cs
@@ -1,15 +1,30 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using HairdresserProject.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+// Veritabaný baðlantýsýný burada yapýyoruz
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Identity'yi ekliyoruz
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
 
 var app = builder.Build();
+
+// Uygulamanýzýn geri kalan konfigürasyonlarý
+app.MapControllers();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -20,6 +35,7 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+// Route yapýlandýrmasý
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
